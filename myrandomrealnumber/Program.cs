08aec3f5c7a33b51e90fe4e717226f2bbc2735f9
sugyeong Kim <s2018s25@e-mirim.hs.kr>
2020-05-19 22:36:35 +0900

    5번 문제 random 사용

--- myrandomrealnumber/Program.cs
+++ myrandomrealnumber/Program.cs
@@ -10,7 +10,31 @@ namespace myrandomrealnumber
     {
         static void Main(string[] args)
         {
-           
+            Random random = new Random();
+            int num = random.Next(0, 200), user;
+            Console.WriteLine(num);
+
+            while (true)
+            {
+                Console.Write("숫자를 입력해주세요: ");
+                user = int.Parse(Console.ReadLine());
+
+                if (user > num)
+                {
+                    Console.WriteLine(user + "보다는 작습니다");
+                }
+                else if (user < num)
+                {
+                    Console.WriteLine(user + "보다는 큽니다");
+                }
+                else
+                {
+                    Console.WriteLine("정답입니다~!");
+                    break;
+                }
+
+                Console.WriteLine("");
+            }
         }
     }
 }
